dcd66d0469542c6192a2d4adb2e07cc52919219a
Azamat Iskanderov <IskanderovAk98@gmail.com>
2023-02-24 17:42:21 +0300

    create web bianary page

---
+++ index.js
@@ -0,0 +1,45 @@
+class BinarySearchTree {
+  constructor(container) {
+    this.container = container;
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = { value, left: null, right: null };
+
+    if (this.root === null) {
+      this.root = newNode;
+      this.drawNode(newNode);
+      return;
+    }
+
+    let currentNode = this.root;
+
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          this.drawNode(newNode);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          this.drawNode(newNode);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  drawNode(node) {
+    const nodeElement = document.createElement("div");
+    nodeElement.classList.add("node");
+    nodeElement.innerText = node.value;
+    this.container.appendChild(nodeElement);
+  }
+}
